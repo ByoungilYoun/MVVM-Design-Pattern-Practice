57c8f7f73d544c997fe298b43e006540cc9aaddd
ByoungilYoun <jimmy7525@naver.com>
2020-11-05 23:38:25 +0900

    Set Navi & Basic TableView

--- MVVMExample/MVVMExample/ViewController.swift
+++ MVVMExample/MVVMExample/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
+  private let tableView : UITableView = {
+    let table = UITableView()
+    table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    return table
+  }()
+  
+  //MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    setNavi()
+    configureUI()
   }
+  
+  //MARK: - setNavi()
+  private func setNavi() {
+    navigationItem.title = "Home"
+  }
+  
+  //MARK: - configureUI()
+  private func configureUI() {
+    view.addSubview(tableView)
+    tableView.dataSource = self
+    tableView.frame = view.bounds
+  }
+}
 
-
+  //MARK: - UITableViewDataSource
+extension ViewController : UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.textLabel?.text = "Hello World"
+    return cell
+  }
 }
 
